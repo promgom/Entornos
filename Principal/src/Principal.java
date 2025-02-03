1d20825a63e6cdb0266d3ee868ffcdf9e3913334
promerog <romerogombau@gmail.com>
2025-02-03 11:53:56 +0100

    Cambios en el código

--- Principal/src/Principal.java
+++ Principal/src/Principal.java
@@ -3,21 +3,23 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Principal {
+    static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Hola mundo.");
 
         System.out.println("Fecha y hora actual: " + obtenerFechaHora());
 
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese la cantidad de usuarios: ");
-        int cantidad = scanner.nextInt();
-        scanner.nextLine(); 
+        int cantidad = teclado.nextInt();
+        teclado.nextLine();
 
         Usuario[] usuarios = crearUsuarios(cantidad);
 
-        System.out.println("\nUsuarios creados:");
+        System.out.println("--------------------");
+        System.out.println("Usuarios");
         for (Usuario usuario : usuarios) {
-            System.out.println("Nombre: " + usuario.nombre + ", Apellidos: " + usuario.apellidos + ", Email: " + usuario.email);
+            System.out.println("Nombre: " + usuario.nombre + "\nApellidos: " + usuario.apellidos + "\nEmail: " + usuario.email);
         }
     }
 
@@ -28,17 +30,16 @@ public class Principal {
     }
 
     public static Usuario[] crearUsuarios(int cantidad) {
-        Scanner scanner = new Scanner(System.in);
         Usuario[] usuarios = new Usuario[cantidad];
 
         for (int i = 0; i < cantidad; i++) {
             usuarios[i] = new Usuario();
-            System.out.print("Ingrese el nombre del usuario " + (i + 1) + ": ");
-            usuarios[i].nombre = scanner.nextLine();
-            System.out.print("Ingrese los apellidos del usuario " + (i + 1) + ": ");
-            usuarios[i].apellidos = scanner.nextLine();
-            System.out.print("Ingrese el email del usuario " + (i + 1) + ": ");
-            usuarios[i].email = scanner.nextLine();
+            System.out.print("Nombre del usuario "+(i+1)+": ");
+            usuarios[i].nombre = teclado.nextLine();
+            System.out.print("Apellidos del usuario "+(i+1)+": ");
+            usuarios[i].apellidos = teclado.nextLine();
+            System.out.print("Email del usuario " + (i + 1) + ": ");
+            usuarios[i].email = teclado.nextLine();
         }
         return usuarios;
     }
